09521d9b2d6f962695f3e4bdc860a948d4927ca5
Jenil1105 <unselfstudy@gmail.com>
2026-04-10 22:51:38 +0530

    Add user routes

--- backend/src/controllers/userControlller.js
+++ backend/src/controllers/userControlller.js
@@ -12,3 +12,25 @@ exports.createUser = async (req, res) => {
         res.status(400).json({ error: error.message });
     }
 }
+
+exports.getAllUsers = async(req, res) => {
+    try{
+        const users = await User.find();
+        res.status(200).json(users);
+    }catch(error){
+        res.status(500).json({error: error.message});
+    }
+}
+
+exports.getUser = async(req, res)=>{
+    try{
+        const id = req.params.id;
+        const user = await User.findById(id);
+        if(!user){
+            return res.status(404).json({message: "User not found"});
+        }
+        res.status(200).json(user);
+    }catch(error){
+        res.status(500).json({error: error.message});
+    }
+}
--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const userController = require("../controllers/userControlller");
 
 router.post('/', userController.createUser);
+router.get('/', userController.getAllUsers);
+router.get('/:id', userController.getUser);
 
 module.exports = router;
